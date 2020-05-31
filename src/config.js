4241711186d1e02506baae6f2f246719728b3667
azkarmoulana <azkar.moulana@gmail.com>
2020-05-31 22:49:41 +0530

    added stripe publishable key to configs

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  STRIPE_KEY: "pk_test_NpkDGbKoaqlkAnqPAbqN6Nhg00orb8T7Ce",
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-sls-app-attachments",
